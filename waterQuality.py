2590d747f0c81bc4f9a7d0185481e1c93a75eda7
Shahriar Parvej <31899201+Shahriar404@users.noreply.github.com>
2023-10-01 12:21:34 +0600

    Create waterQuality.py

---
+++ waterQuality.py
@@ -0,0 +1,9 @@
+import random
+import numpy as np
+from deap import base, creator, tools, algorithms
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+# Load the dataset with missing values denoted as 'NA'
+data = pd.read_csv('/kaggle/input/water-quality/WaterQuality.csv', na_values='NA')
