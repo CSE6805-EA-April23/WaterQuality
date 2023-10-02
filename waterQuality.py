3caaa5de0a8d94ff4af0eb05493e15b0d7ab36fe
Shahriar Parvej <31899201+Shahriar404@users.noreply.github.com>
2023-10-02 23:25:37 +0600

    Update waterQuality.py

--- waterQuality.py
+++ waterQuality.py
@@ -7,3 +7,18 @@ import pandas as pd
 
 # Load the dataset with missing values denoted as 'NA'
 data = pd.read_csv('WaterQuality.csv', na_values='NA')
+
+# Split the dataset into two subsets based on the target variable
+subset_with_target_1 = data[data['EVENT'] == 1]
+subset_with_target_0 = data[data['EVENT'] == 0]
+
+# Calculate the average values for each column in both subsets
+subset_with_target_1_means = subset_with_target_1.mean()
+subset_with_target_0_means = subset_with_target_0.mean()
+
+# Fill missing values in each subset with the corresponding average values
+subset_with_target_1.fillna(subset_with_target_1_means, inplace=True)
+subset_with_target_0.fillna(subset_with_target_0_means, inplace=True)
+
+# Merge the two subsets back into one dataset
+filled_data = pd.concat([subset_with_target_1, subset_with_target_0])
